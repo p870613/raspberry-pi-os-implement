3404427b49ef09c28dc3a825de32e081f6b362d7
p870613 <p870613@yahoo.com.tw>
2022-06-02 00:02:00 +0800

    get_core_time

--- src/libs/time.c
+++ src/libs/time.c
@@ -1,6 +1,11 @@
 #include <time.h>
+#include <type.h>
 
 float get_core_time() {
     float ret;
+    size_t count, freq;
+    asm volatile("mrs %[result], cntpct_el0": [result]"=r"(count));
+    asm volatile("mrs %[result], cntfrq_el0": [result]"=r"(freq));
+    ret = (1000*count) / freq;
     return ret;
 }
