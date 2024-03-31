60222696a889f8f889bd438e9d3a6ce2c7ef8f9e
p870613 <p870613@yahoo.com.tw>
2024-03-31 23:34:53 +0800

    Fix 'run' command SError due to ./a.out

--- src/libs/cpio.c
+++ src/libs/cpio.c
@@ -22,11 +22,14 @@ void get_file_address(size_t* file_address, size_t address) {
     *file_address = address;
 }
 
-void padding(size_t* address) {
-    char** t = (char*)address;
-    while(**t == '\0'){
-        *t = *t + 1;
+int padding(char* address) {
+    char* t = address;
+    int ret = 0;
+    while(*t == '\0') {
+        ret ++;
+        t = t + 1;
     }
+    return ret;
 }
 
 int check_cpio_end(size_t name_size, size_t address) {
@@ -49,31 +52,28 @@ void cpio_parse(size_t address){
     size_t file_size, name_size, file_address, name_address;
     
     while(1) {
-       printf("%d \n", index);
        get_name_size(&name_size, cur_address);
        get_file_size(&file_size, cur_address);
+
+       if(check_cpio_end(name_size, file_address) == 0)
+           break;
 
        cur_address = cur_address + HEADER_LEN;
        
        get_name_address(&name_address, cur_address);
        cur_address = cur_address + name_size;
-       padding(&cur_address);
+
+       cur_address = cur_address + padding(cur_address);
 
        get_file_address(&file_address, cur_address);
        cur_address = cur_address + file_size;
-       padding(&cur_address);
+       cur_address = cur_address + padding(cur_address);
        
-       if(check_cpio_end(name_size, file_address) == 0){
-           printf("--");
-           break;
-       }
-
        address = cur_address;
        assing_to_struct(index, address, file_size, name_size, file_address, name_address);
        index ++;
     }
     header_index = index;
-    printf("%d \n", header_index);
 }
 
 void get_content(size_t address, size_t offset, char* buf){
@@ -107,8 +107,9 @@ void get_file_content(char* filename) {
 
 void* load_file(char* filename, size_t filename_size) {
     for(int i = 0; i < header_index; i++) {
-        if (strncmp(header[i].name_address, filename, filename_size)) {
+        if (!strncmp(header[i].name_address, filename, filename_size)) {
             return (void*)header[i].file_address;
         }
     }
+    return NULL;
 }
